823c03ed7501ff5d329823dd334909724bd7a6c6
EC2 Default User <ec2-user@ip-172-31-30-37.us-east-2.compute.internal>
2020-09-08 20:23:43 +0000

    pass all test

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,69 @@
 # When done, submit this entire file to the autograder.
 
 # Part 1
-
 def sum arr
-  # YOUR CODE HERE
+	arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	if arr.empty?
+		return 0 
+	elsif arr.length == 1
+		return arr.first
+	else
+		arr.sort!
+		return arr[-1] + arr[-2] 
+	end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	if arr.empty? or arr.length == 1
+		return false
+	else
+		iterations = (arr.length/2).ceil
+		arr[0..iterations].each do |num1|
+	arr.each do |num2|
+		if num1 + num2 == n
+	  		return true
+		end
+		end
+		end
+	end
+	return false
 end
 
-# Part 2
 
+# Part 2
 def hello(name)
-  # YOUR CODE HERE
+	return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	s =~ /^(?=\w)([^?=aeiou])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	if (s =~ /^[01]+$/).nil?
+		return false
+	else
+		dec_s = s.to_i(10)
+		return dec_s % 4 == 0
+	end
+	
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	attr_accessor :price, :isbn
+  
+	def initialize(isbn, price)
+	raise ArgumentError if isbn.empty? or price <= 0
+	@isbn = isbn
+	@price = price
+	end
+
+	def price_as_string
+	"$" + '%.2f' % price
+	end
 end
